66283c25c1a44a3368ff0aba8874729c0119b079
voa-rio <guilherme.tcordeir@gmail.com>
2026-09-17 13:44:17 -0300

    Add functions to save and retrieve language preference

--- js/idioma.js
+++ js/idioma.js
@@ -1,1 +1,9 @@
+const idiomas = ["pt", "en", "es"];
 
+function salvarIdioma(idioma) {
+    localStorage.setItem("idioma", idioma);
+}
+
+function obterIdioma() {
+    return localStorage.getItem("idioma") || "pt";
+}
